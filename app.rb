a54a2a7d1e9fd126e0051f72abba7646bd6767e5
Sergey Evstifeev <sergey.evstifeev@klarna.com>
2016-12-03 17:51:24 +0100

    Serve playlist

--- app.rb
+++ app.rb
@@ -18,6 +18,7 @@ class Beak < Sinatra::Base
 
     set(:assets_folder_name) { 'public' }
     set(:public_folder) { File.join(Dir.pwd, settings.assets_folder_name) }
+    set(:recordings_folder) { '/tmp/track_entries' }
   end
 
   error do
@@ -34,6 +35,14 @@ class Beak < Sinatra::Base
 
   get '/ping' do
     'PONG'
+  end
+
+  get '/api/recordings/:record_id/playlist' do
+    playlist_files = Dir[File.join(settings.recordings_folder, params['record_id'], '*')]
+    playlist_files.sort!
+
+    content_type :json
+    playlist_files.map { |f| File.basename(f, ".*") }.to_json
   end
 
   get '*' do
