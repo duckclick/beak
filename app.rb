37fc195f30fc797dbd0eaacbd8b2a5985e475983
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-03-15 19:41:29 +0100

    return only css links instead of full head

--- app.rb
+++ app.rb
@@ -63,7 +63,15 @@ class Beak < Sinatra::Base
     track_entry = JSON.parse(settings.redis.hget(params['record_id'], params['id']))
     frame_html =  Nokogiri::HTML(track_entry['markup'])
 
-    head = frame_html.css('head').inner_html.strip
+    title = frame_html.css('head/title').inner_html.strip
+    links = frame_html.css('head/link').select { |l| l.get_attribute('rel') == 'stylesheet' }
+    links_checksum = Digest::MD5.hexdigest(links.map(&:to_s).sort.join)
+
+    links_with_id = links.map do |l|
+      l.set_attribute('class', "link-#{links_checksum}")
+      l.to_s
+    end
+
     body = frame_html.css('body')
     body_attributes = body.first
       .attributes
@@ -72,7 +80,11 @@ class Beak < Sinatra::Base
 
     content_type :json
     {
-      head: head,
+      head: {
+        title: title,
+        links: links_with_id,
+        links_checksum: links_checksum
+      },
       body: body.inner_html.strip,
       body_attributes: body_attributes
     }.to_json
